48910896d1209b3b4262dde073e57b83ab98e6c2
Daniel Lo Nigro <git@d.sb>
2024-08-07 12:15:04 -0700

    Use primary constructor in AngleControl
    Signed-off-by: Daniel Lo Nigro <git@d.sb>

--- src/WebCamControl.Core/AngleControl.cs
+++ src/WebCamControl.Core/AngleControl.cs
@@ -3,24 +3,23 @@ namespace WebCamControl.Core;
 /// <summary>
 /// Wrapper around <see cref="ICameraControl"/> that treats the value like an angle.
 /// </summary>
-public class AngleControl : ICameraControl<float>
+public class AngleControl(ICameraControl<int> control) : ICameraControl<float>
 {
 	private const float _arcsecondsInDegree = 3600;
-	
-	private readonly ICameraControl<int> _control;
 
-	public AngleControl(ICameraControl<int> control)
-	{
-		_control = control;
-	}
-
-	public float Minimum => _control.Minimum / _arcsecondsInDegree;
-	public float Maximum => _control.Maximum / _arcsecondsInDegree;
-	public float Step => _control.Step / _arcsecondsInDegree;
+	public string Name => control.Name;
+	public float Minimum => control.Minimum / _arcsecondsInDegree;
+	public float Maximum => control.Maximum / _arcsecondsInDegree;
+	public float Step => control.Step / _arcsecondsInDegree;
 
 	public float Value
 	{
-		get => _control.Value / _arcsecondsInDegree;
-		set => _control.Value = (int)(value * _arcsecondsInDegree);
+		get => control.Value / _arcsecondsInDegree;
+		set => control.Value = (int)(value * _arcsecondsInDegree);
+	}
+
+	public static AngleControl? CreateIfNotNull(ICameraControl<int>? control)
+	{
+		return control == null ? null : new AngleControl(control);
 	}
 }
